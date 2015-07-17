0834cebb4a03fea0f47d7c4a0e21a21c58c6413b
luisantonioig <c-remix@hotmail.es>
2015-07-16 22:45:07 -0500

    El formulario de comentarios es mandado a un correo

--- web2py/applications/CabanasDonAurelio/controllers/usuario.py
+++ web2py/applications/CabanasDonAurelio/controllers/usuario.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 # intente algo como
+from gluon.tools import Mail
+mail = Mail()
+mail.settings.server = 'smtp.gmail.com:587'
+mail.settings.sender = 'antonio.ibarra.g46@gmail.com'
+mail.settings.login = 'antonio.ibarra.g46:qnzsymccqgnogxyg'
+
 def index(): return dict(message="hello from usuario.py")
 
 def info():
     return dict(hola="hola")
+
+def send_email():
+    mensaje = 'Nombre: '+request.vars.nombre+' '+request.vars.apellidos+'\ne-Mail: '+request.vars.email+ '\nComentario: '+request.vars.comentarios
+    if mail.send(to=['c-remix@hotmail.es'],
+          subject='Comentarios de "Cabañas Don Aurelio"',
+          # If reply_to is omitted, then mail.settings.sender is used
+          message=mensaje):
+        session.flash = "Mensaje enviado"
+    else:
+        session.flash= "No se pudo enviar el mensaje"
+    redirect(URL('info'))
