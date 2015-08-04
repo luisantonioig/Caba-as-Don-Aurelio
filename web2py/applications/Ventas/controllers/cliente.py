49eb57e73bf18987c4d036e18a72fcd2e16b48ac
luisantonioig <c-remix@hotmail.es>
2015-08-04 13:29:10 -0500

    Ver los clientes registrados

--- web2py/applications/Ventas/controllers/cliente.py
+++ web2py/applications/Ventas/controllers/cliente.py
@@ -5,3 +5,7 @@ def index(): return dict(message="hello from cliente.py")
 def nuevo():
     formulario = SQLFORM(db.cliente)
     return dict(Nuevo_cliente = formulario)
+
+def ver_clientes():
+    grid = SQLFORM.grid(db.cliente)
+    return dict(grid=grid)
