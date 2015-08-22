c1444086340aa00e3393943881438d2b980e9641
luisantonioig <c-remix@hotmail.es>
2015-08-22 12:05:32 -0500

    Ya guarda clientes

--- web2py/applications/Ventas/controllers/cliente.py
+++ web2py/applications/Ventas/controllers/cliente.py
@@ -4,6 +4,12 @@ def index(): return dict(message="hello from cliente.py")
 
 def nuevo():
     formulario = SQLFORM(db.cliente)
+    if formulario.process().accepted:
+        response.flash = 'Ok!!'
+    elif formulario.errors:
+        response.flash = 'Tienes errores en los datos'
+    else:
+        response.flash = 'Por fabor llena los datos del cliente'
     return dict(Nuevo_cliente = formulario)
 
 def ver_clientes():
